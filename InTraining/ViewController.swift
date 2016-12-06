d8751937eab284d2659b70c6ca86926419e1ba77
ADMINISTRADORUTM <ADMINUTM@iMac-de-TecMilenio.local>
2016-12-06 09:47:16 -0600

    Initial Commit

---
+++ InTraining/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InTraining
+//
+//  Created by ADMINISTRADORUTM on 06/12/16.
+//  Copyright © 2016 CAHD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
